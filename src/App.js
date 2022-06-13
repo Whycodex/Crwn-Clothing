b70af816754ecde794c9562fc51b0f47b8193cc0
Whycodex <neerajgupta12300@gmail.com>
2022-06-14 03:51:51 +0530

    added checkout functionality with required modification and set up of firebase database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import {Routes, Route, Outlet} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
-import CheckOut from './routes/checkout/checkput.component';
+import CheckOut from './routes/checkout/checkout.component';
 
 const App = () => {
   return (
